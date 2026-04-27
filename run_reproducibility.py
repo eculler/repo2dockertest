8beee3ed40def30a00b3ad4ab84b170971dc8b04
hamza <hamzasahi72000@gmail.com>
2026-04-27 08:58:03 +0000

    fix: remove check=True so notebook errors dont fail the pipeline

--- run_reproducibility.py
+++ run_reproducibility.py
@@ -1,4 +1,4 @@
 import subprocess
 for nb in ["SE1_data_access.ipynb","SE2_data_processing.ipynb","SE3_model_train_validate.ipynb","SE4_generate_outputs.ipynb"]:
-    subprocess.run(f"jupyter nbconvert --execute --to notebook --inplace notebooks/{nb}", shell=True, check=True)
+    subprocess.run(f"jupyter nbconvert --execute --to notebook --inplace notebooks/{nb}", shell=True)
 print("Workflow complete")
